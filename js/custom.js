8d457f8c285932b5146517aa2c7718412c3ed875
Nandozer <nando.erni@outlook.de>
2018-05-15 13:45:49 +0200

    Ein guter Push

--- js/custom.js
+++ js/custom.js
@@ -1,16 +1,33 @@
 var output = document.getElementById("slidOutput");
 var slider = document.getElementById('slider');
-var reader = new FileReader();
 const finanzausgleichPath = "data/Gesamtauswirkungen_Finanzausgleich.csv";
-var dataArray, dataString;
+var dataArray = [], dataString, dataArrayRows, arrayTable = [];
 
 function updateSlider() {
     output.innerHTML = slider.value;
+
+}
+
+function updateSliderTable() {
+    var year = slider.value;
+    var arrIndex = 0;
+    //alert(dataArray[0][2] + " -- " + year.toString());
+    for(var i = 0; i < dataArray.length; i++){
+
+        if(dataArray[i][2] == year.toString()){
+            arrayTable[arrIndex++] = dataArray[i];
+        }
+    }
 }
 
 function readingFinanzausgleich() {
+
     readTextFile(finanzausgleichPath);
-    alert(dataString);
+    dataArrayRows = d3.csvParseRows(dataString);
+
+    for(var i = 0; i < dataArrayRows.length; i++){
+        dataArray[i] = string2Array(dataArrayRows[i].toString(), ';');
+    }
 }
 
 function readTextFile(file)
@@ -26,6 +43,32 @@ function readTextFile(file)
                 dataString = rawFile.responseText;
             }
         }
-    }
+    };
     rawFile.send(null);
+}
+
+function string2Array(str, a) {
+    var arr = [];
+    var index = 0;
+    arr[index] = "";
+    for(var i = 0; i < str.length; i++){
+        var cur=str[i];
+        if(cur == a){
+            index++;
+            arr[index] = "";
+        } else {
+            arr[index] += cur;
+        }
+    }
+    return arr;
+}
+
+function sliderOninput() {
+    updateSlider();
+    updateSliderTable();
+}
+
+function bodyOnload(){
+    readingFinanzausgleich();
+    updateSlider();
 }
--- js/onEvents.js
+++
@@ -1,7 +0,0 @@
-function sliderOninput() {
-    updateSlider();
-}
-
-function bodyOnload(){
-    updateSlider();
-}
