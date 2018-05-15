1482f4cb4c653109100a9b89066e18f565e7e20b
nandozer <nando.erni@outlook.de>
2018-05-15 20:31:17 +0200

    Ein guter Push in die richtige Richtung!

--- js/custom.js
+++ js/custom.js
@@ -1,23 +1,22 @@
 var output = document.getElementById("slidOutput");
 var slider = document.getElementById('slider');
 const finanzausgleichPath = "data/Gesamtauswirkungen_Finanzausgleich.csv";
-var dataArray = [], dataString, dataArrayRows, arrayTable = [];
+var dataArray = [], dataString, dataArrayRows, sliderArray = [], tableArray = [];
 
 function updateSlider() {
     output.innerHTML = "Jahr: " + slider.value;
 
 }
 
-function updateSliderTable() {
+function updateArraySlider() {
     var year = slider.value;
     var arrIndex = 0;
     for(var i = 0; i < dataArray.length; i++){
 
         if(dataArray[i][2] == year.toString()){
-            arrayTable[arrIndex++] = dataArray[i];
+            sliderArray[arrIndex++] = dataArray[i];
         }
     }
-    generateTable(arrayTable);
 }
 
 function readingFinanzausgleich() {
@@ -67,54 +66,55 @@ function string2Array(str, a) {
 
 function sliderOninput() {
     updateSlider();
-    updateSliderTable();
+    updateArraySlider();
 }
 
 function bodyOnload(){
     readingFinanzausgleich();
     updateSlider();
-    //images laden nicht immer
-    updateGemeinde("Sulgen", "img/gemeinden/Sulgen.png");
+    updateGemeinde("Aadorf", "img/gemeinden/Sulgen.png");
+
 }
 
 function generateTable(data) {
-    var html = '';
+    var table = document.getElementById("dataTable");
+    var html = "";
 
-    if (typeof (data[0]) === 'undefined') {
-        return null;
+    //BFS_NR_GEMEINDE GEMEINDE_NAME Jahr Auszahlung_Abschoepfung_in_CHF Auszahlung_Abschoepfung_in_CHF_pro_Einwohner
+    html += "<thead>\n" +
+        "    <tr>\n" +
+        "      <th scope=\"col\">BFS_NR_GEMEINDE</th>\n" +
+        "      <th scope=\"col\">GEMEINDE_NAME</th>\n" +
+        "      <th scope=\"col\">Jahr</th>\n" +
+        "      <th scope=\"col\">Auszahlung_Abschoepfung_in_CHF</th>\n" +
+        "      <th scope=\"col\">Auszahlung_Abschoepfung_in_CHF_pro_Einwohner</th>\n" +
+        "    </tr>\n" +
+        "  </thead>";
+
+    html += "<tbody>";
+    for(var i = 0; i < data.length; i++){
+
+    html += "<tr>\n" +
+        "      <th scope=\"row\">"+data[i][0]+"</th>\n" +
+        "      <td>"+data[i][1]+"</td>\n" +
+        "      <td>"+data[i][2]+"</td>\n" +
+        "      <td>"+data[i][3]+"</td>\n" +
+        "      <td>"+data[i][4]+"</td>\n" +
+        "    </tr>";
+
     }
-
-    if (data[0].constructor === String) {
-        html += '<tr>\r\n';
-        for (var item in data) {
-            html += '<td>' + data[item] + '</td>\r\n';
-        }
-        html += '</tr>\r\n';
-    }
-
-    if (data[0].constructor === Array) {
-        for (var row in data) {
-            html += '<tr>\r\n';
-            for (var item in data[row]) {
-                html += '<td>' + data[row][item] + '</td>\r\n';
-            }
-            html += '</tr>\r\n';
-        }
-    }
-
-    if (data[0].constructor === Object) {
-        for (var row in data) {
-            html += '<tr>\r\n';
-            for (var item in data[row]) {
-                html += '<td>' + item + ':' + data[row][item] + '</td>\r\n';
-            }
-            html += '</tr>\r\n';
-        }
-    }
-
-    return html;
+    html += "</tbody>";
+    table.innerHTML = html;
 }
 
 function updateGemeinde(gemeinde, gemeindeImg) {
     document.getElementById("gemeindeH").innerHTML = "<img id='gemeindeLogo' src='"+ gemeindeImg+"'> " + gemeinde;
+    var arrIndex = 0;
+    for(var i = 0; i < dataArray.length; i++){
+
+        if(dataArray[i][1] == gemeinde){
+            tableArray[arrIndex++] = dataArray[i];
+        }
+    }
+    generateTable(tableArray);
 }
